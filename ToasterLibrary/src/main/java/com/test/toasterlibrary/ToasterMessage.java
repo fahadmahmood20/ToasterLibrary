63c15b64654f636245fffecc7583f8719f3a19f4
Usman Asghar <musmanasghar9@gmail.com>
2020-12-25 09:29:34 +0500

    com

--- ToasterLibrary/src/main/java/com/test/toasterlibrary/ToasterMessage.java
+++ ToasterLibrary/src/main/java/com/test/toasterlibrary/ToasterMessage.java
@@ -4,9 +4,24 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class ToasterMessage {
+
+    private int a;
     public static void s(Context c, String message){
 
         Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+    }
 
+    public ToasterMessage() {
+        setA();
+    }
+
+
+    private void setA()
+    {
+        a = 5;
+    }
+    public int getA()
+    {
+        return a;
     }
 }
